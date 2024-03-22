26635a50dc26eaa10eb5d2692580897b5592782e
mdmehedi060 <mdmehedi060@gmail.com>
2024-03-22 09:06:34 +0600

    sign in ui desigen

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import SignIn from "./Pages/SignIn/SignIn";
 import SignUp from "./Pages/SignUp/SignUp";
 import Profile from "./Pages/Profile/Profile";
 import About from "./Pages/About/About";
+import Header from "./Components/Header/Header";
 
 function App() {
   
 
   return (
     <BrowserRouter>
-     
+     <Header/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/signin" element={<SignIn/>}></Route>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 
 
 const SignUp = () => {
     return (
-        <div>
-           <h3>This is SignUp Pages</h3> 
+        <div className="p-3 max-w-lg mx-auto">
+           <h3 className="text-3xl text-center font-semibold my-7">SignUp </h3>
+           <form className="flex flex-col gap-4">
+            <input type="text" placeholder="UserName" className="border p-3 rounded-lg" id="username" />
+            <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
+            <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" />
+            <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">Signup</button>
+            </form> 
+            <div className="flex gap-2 mt-5">
+                <p>Have an Account?</p>
+                <Link to={"/signin"}>
+                    <span className="text-blue-700">Signin</span>
+                </Link>
+            </div>
         </div>
     );
 };
